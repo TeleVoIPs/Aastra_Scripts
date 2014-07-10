9fd11a481c4479abfa65f5242fe055efb48c069f
David Maldonado <david.m.maldonado@gmail.com>
2014-07-10 14:03:25 -0400

    modified startup.php

--- 2.3.1-1.noarch/var/www/html/aastra/asterisk/startup.php
+++ 2.3.1-1.noarch/var/www/html/aastra/asterisk/startup.php
@@ -507,6 +507,10 @@ switch($action)
 					if($nb_softkeys==6) $object->addSoftkey('3',Aastra_get_label('Reboot',$language),$XML_SERVER.'&action=reboot');
 					else $object->addSoftkey('6',Aastra_get_label('Reboot',$language),$XML_SERVER.'&action=reboot');
 					}
+				if($header['model']=='Aastra6867i') {
+					$object->addSoftkey('1',Aastra_get_label('Backspace',$language),'SoftKey:BackSpace');
+					$object->addSoftkey('2',Aastra_get_label('Submit',$language),'SoftKey:Submit');
+				}
 				if($nb_softkeys==6) 
 					{
 					$object->addSoftkey('1',Aastra_get_label('Backspace',$language),'SoftKey:BackSpace');
